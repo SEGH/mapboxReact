c3322046b7a71cfcec4bcb0393ed91eafe4d6713
Susan Holland <segh@fastmail.com>
2021-06-15 19:45:28 -0400

    Add geolocation control

--- src/App.js
+++ src/App.js
@@ -12,25 +12,39 @@ function App() {
   const marker = useRef(null);
   const [markerLng, setMarkerLng] = useState(-75.1684);
   const [markerLat, setMarkerLat] = useState(39.9322);
+  const geolocate = useRef(null);
+  const [geoCoords, setGeoCoords] = useState(null);
 
   useEffect(() => {
     if (map.current) return;
+    console.log('useEffect One')
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom
     });
+
     if (marker.current) return;
     marker.current = new mapboxgl.Marker({
       color: "#FFFFFF",
       draggable: true
     }).setLngLat([markerLng, markerLat]);
     marker.current.addTo(map.current);
+
+    if (geolocate.current) return;
+    geolocate.current = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    })
+    map.current.addControl(geolocate.current);
   });
 
   useEffect(() => {
     if (!map.current) return;
+    console.log('useEffect Two')
     map.current.on('move', () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
@@ -41,6 +55,11 @@ function App() {
       setMarkerLng(marker.current.getLngLat().lng.toFixed(4));
       setMarkerLat(marker.current.getLngLat().lat.toFixed(4));
     });
+    if (!geolocate.current) return;
+    geolocate.current.on('geolocate', (data) => {
+      console.log(data);
+      setGeoCoords({ lat: data.coords.latitude.toFixed(4), lon: data.coords.longitude.toFixed(4)});
+    })
   });
 
   return (
@@ -49,6 +68,7 @@ function App() {
       <div className="sidebar">
         <p>Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}</p>
         <p>Marker Longitude: {markerLng} | Latitude: {markerLat}</p>
+        <p>{geoCoords ? `Current Longitude: ${geoCoords.lon} | Latitude: ${geoCoords.lat}` : ""}</p>
       </div>
     </div>
   );
